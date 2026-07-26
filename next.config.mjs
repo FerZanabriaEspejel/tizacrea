b044c6c17c90ef29ce7756eb7cf68acdf707b1aa
FerZanabriaEspejel <zanamari1125@gmail.com>
2026-07-26 15:26:53 -0600

    carousel 3

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  allowedDevOrigins: ["192.168.1.17",
+  allowedDevOrigins: [
+    "192.168.1.17",
     "172.21.96.1",
     "localhost",
-    ],
+  ],
 
   images: {
     remotePatterns: [
       {
         protocol: "https",
         hostname: "images.unsplash.com",
+      },
+      {
+        protocol: "https",
+        hostname: "rokpnqnnedkuuxasmrtp.supabase.co",
       },
     ],
   },
